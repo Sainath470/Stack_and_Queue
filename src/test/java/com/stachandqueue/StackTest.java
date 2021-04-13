5e79c5737e2a3d0d7796065fdb60f82b5df177d2
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-13 21:53:55 +0530

    ADDED : test case to create a stack

---
+++ src/test/java/com/stachandqueue/StackTest.java
@@ -0,0 +1,26 @@
+package com.stachandqueue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTest {
+    @Test
+    public void given3IntegersWhenAddedShouldBeLast(){
+        Node<Integer> firstNode = new Node<>(70);
+        Node<Integer> secondNode = new Node<>(30);
+        Node<Integer> thirdNode = new Node<>(56);
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(firstNode);
+        stack.push(secondNode);
+        stack.push(thirdNode);
+        stack.printStack();
+
+        INode<Integer> peak = stack.peak();
+        Assertions.assertEquals(thirdNode, peak);
+    }
+
+}
